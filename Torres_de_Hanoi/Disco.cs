710420ea75656d24d886fdcd409724c196d26a06
Laura <laumega1@epsg.upv.es>
2020-02-17 16:24:03 +0100

    Clase disco

---
+++ Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Disco
+    {
+        public int Valor { get; set; }
+
+        public Disco(int valor)
+        {
+            Valor = valor;
+        }
+
+    }
+}
---
+++ Torres_de_Hanoi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Disco> listaDiscos = new List<Disco>();
+            // Pedimos que introduzca cuantos discos quiere que hayan
+            int numeroDiscos = 0;
+
+            while (numeroDiscos == 0)
+            {
+                Console.WriteLine("¿Cuantos discos quiere tener?");
+                if(!Int32.TryParse(Console.ReadLine(), out numeroDiscos))
+                {
+                    Console.WriteLine("Introduzca un número");
+                }
+            }
+            Console.WriteLine("hola");
+
+            // Creamos el número de discos que se han pedido y los guardamos en un array
+            for (int i = 0; i < numeroDiscos; i++)
+            {
+                // Creamos un disco
+                Disco disco = new Disco(i+1);
+
+                // Añadimos el disco a la lista
+                listaDiscos.Add(disco);
+            }
+
+            Console.WriteLine(listaDiscos[0].Valor);
+
+        }
+    }
+}
